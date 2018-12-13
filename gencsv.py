329ee5b2857515a9205eea95f77fede6e6c82d13
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-13 20:13:07 +0900

    Generate TSV instead of CSV

--- gencsv.py
+++ gencsv.py
@@ -18,11 +18,11 @@ def main():
 
     for (dirname, category), infos in results.items():
         keys = [k for k, _ in infos[0]]
-        with open('%s/%s.csv' % (dirname, category), 'w') as f:
-            f.write(','.join(keys) + '\n')
+        with open('%s/%s.tsv' % (dirname, category), 'w') as f:
+            f.write('\t'.join(keys) + '\n')
             for info in infos:
                 values = [str(v) for _, v in info]
-                f.write(','.join(values) + '\n')
+                f.write('\t'.join(values) + '\n')
 
 
 if __name__ == '__main__':
