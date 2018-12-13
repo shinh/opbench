c9159eaa30d72a7d4e48f24ec44cb98d70d0bdbe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-13 12:30:04 +0900

    Introduce Task class to share it with an exporter

--- bench.py
+++ bench.py
@@ -1,33 +1,12 @@
-import glob
-import importlib
-import os
-
-
-def import_file(filename):
-    module_name = filename[:-3].replace('/', '.')
-    return importlib.import_module(module_name)
-
-
-def collect_all_tasks():
-    task_pys = sorted(glob.glob('tasks/*.py'))
-    task_pys += sorted(glob.glob('*/tasks/*.py'))
-    tasks = []
-    for task_py in task_pys:
-        module = import_file(task_py)
-        tasks.extend(module.get_tasks())
-    return tasks
+import task as task_lib
 
 
 def main():
-    tasks = collect_all_tasks()
+    tasks = task_lib.collect_all_tasks()
     for task in tasks:
         print(task.name)
-        task.to_gpu()
-        inputs = task.inputs()
-        if not isinstance(inputs, tuple):
-            inputs = [inputs]
-        inputs = [task.xp.array(input) for input in inputs]
-        outputs = task(*inputs)
+        task.run()
 
 
-main()
+if __name__ == '__main__':
+    main()
---
+++ task.py
@@ -0,0 +1,46 @@
+import glob
+import importlib
+import os
+
+import chainer
+
+
+class Task(object):
+    def __init__(self, model):
+        """Initializes the task object.
+
+        Args:
+          model: A `chainer.Chain` with additional required attributes:
+            name: A str of the task name.
+            inputs: A np.array or a tuple of np.array objects to be
+              fed to `forward` function.
+        """
+        assert isinstance(model, chainer.Chain), model
+        assert hasattr(model, 'inputs')
+        self.name = model.name
+        self.model = model
+
+    def run(self):
+        self.model.to_gpu()
+        inputs = self.model.inputs()
+        if not isinstance(inputs, tuple):
+            inputs = [inputs]
+        inputs = [self.model.xp.array(input) for input in inputs]
+        outputs = self.model(*inputs)
+        return outputs
+
+
+def import_file(filename):
+    module_name = filename[:-3].replace('/', '.')
+    return importlib.import_module(module_name)
+
+
+def collect_all_tasks():
+    task_pys = sorted(glob.glob('tasks/*.py'))
+    task_pys += sorted(glob.glob('*/tasks/*.py'))
+    tasks = []
+    for task_py in task_pys:
+        module = import_file(task_py)
+        for task in module.get_tasks():
+            tasks.append(Task(task))
+    return tasks
