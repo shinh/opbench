eccfce63a3224ac2162261fd717dd85804893f7c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-13 14:26:39 +0900

    Add comments to Driver class

--- driver.py
+++ driver.py
@@ -5,12 +5,9 @@ import numpy as np
 
 
 class Driver(object):
-    def __init__(self):
-        pass
-
     def bench(self, task, time_budget_sec=1.0, max_count=100):
         inputs, expected_outputs = task.run()
-        actual_outputs = self.get_result(task, inputs)
+        actual_outputs = self.run_first(task, inputs)
         np.testing.assert_allclose(expected_outputs, actual_outputs, rtol=1e-2)
 
         times = []
@@ -28,13 +25,31 @@ class Driver(object):
             result.append(times[i + 1] - times[i])
         return result
 
-    def prepare_task(self, task):
-        pass
+    def run_first(self, task, inputs):
+        """Runs the model first time.
 
-    def get_result(self, task):
+        This run is both for testing sanity of the driver and to give
+        a chance for drivers to warm-up the model and stage inputs to
+        the device.
+
+        Args:
+          task: A `Task` object.
+          inputs: A list of np.array objects.
+
+        Returns:
+          A list of np.array objects.
+        """
         raise NotImplementedError(
             '`get_result` must be overridden for %s' % type(self))
 
     def run_task(self, task):
+        """Runs the task once.
+
+        The inputs passed to `run_first` in previous run should be
+        used as the input of the model.
+
+        Args:
+          task: A `Task` object.
+        """
         raise NotImplementedError(
             '`run_task` must be overridden for %s' % type(self))
--- drivers/chainer.py
+++ drivers/chainer.py
@@ -4,18 +4,18 @@ import driver
 
 
 class ChainerDriver(driver.Driver):
-    def run_task(self, task):
-        outputs = task.model(*self.inputs)
-        chainer.cuda.Stream.null.synchronize()
-        return outputs
-
-    def get_result(self, task, inputs):
+    def run_first(self, task, inputs):
         task.model.to_gpu()
         self.inputs = [task.model.xp.array(input) for input in inputs]
         gpu_outputs = self.run_task(task)
         if not isinstance(gpu_outputs, tuple):
             gpu_outputs = [gpu_outputs]
         outputs = [chainer.cuda.to_cpu(v.array) for v in gpu_outputs]
+        return outputs
+
+    def run_task(self, task):
+        outputs = task.model(*self.inputs)
+        chainer.cuda.Stream.null.synchronize()
         return outputs
 
 
