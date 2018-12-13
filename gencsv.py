6f48816ecebad222a8c3e042360537ad4ac5afda
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-13 20:04:44 +0900

    Add a tool to generate CSV from JSONs

---
+++ gencsv.py
@@ -0,0 +1,29 @@
+import glob
+import json
+import os
+
+
+def main():
+    results = {}
+    for js in sorted(glob.glob('results/*/*.json')):
+        with open(js) as f:
+            info = json.load(f)
+
+        dirname = os.path.dirname(js)
+        key, category = info[0]
+        assert key == 'category'
+        if (dirname, category) not in results:
+            results[(dirname, category)] = []
+        results[(dirname, category)].append(info[1:])
+
+    for (dirname, category), infos in results.items():
+        keys = [k for k, _ in infos[0]]
+        with open('%s/%s.csv' % (dirname, category), 'w') as f:
+            f.write(','.join(keys) + '\n')
+            for info in infos:
+                values = [str(v) for _, v in info]
+                f.write(','.join(values) + '\n')
+
+
+if __name__ == '__main__':
+    main()
