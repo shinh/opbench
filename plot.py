c0c51e201808392a2ece955ff98b2f10299b9cea
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-17 15:04:33 +0900

    Add a naive plotter

---
+++ plot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import argparse
+import glob
+import json
+import os
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run benchmark for ops')
+    parser.add_argument('drivers', type=str)
+    parser.add_argument('--dir', type=str, default='results')
+    parser.add_argument('-x', type=str, default='flops')
+    parser.add_argument('-y', type=str, default='gflops')
+    args = parser.parse_args()
+
+    drivers = args.drivers.split(',')
+
+    by_category = {}
+    for driver in drivers:
+        for js in sorted(glob.glob('%s/%s/*.json' % (args.dir, driver))):
+            with open(js) as f:
+                info = dict(json.load(f))
+
+            info['driver'] = driver
+            category = info['category']
+            if category not in by_category:
+                by_category[category] = []
+            by_category[category].append(info)
+
+    for category, infos in by_category.items():
+        for driver in drivers:
+            x = []
+            y = []
+            for info in infos:
+                if info['driver'] != driver:
+                    continue
+                x.append(info[args.x])
+                y.append(info[args.y])
+            plt.scatter(x, y, label=driver)
+        plt.legend(loc='upper left')
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
